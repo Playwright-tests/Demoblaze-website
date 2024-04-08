c012a4d293910f3542aa931e246480a19eab54be
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-04-08 09:22:28 +0200

    Added the BrowserName enum

--- enums/enums.ts
+++ enums/enums.ts
@@ -2,3 +2,10 @@ export enum URL {
 
     HOME_PAGE = 'https://www.demoblaze.com/index.html'
 }
+
+export enum BrowserName {
+
+    CHROMIUM = 'CHROMIUM',
+    FIREFOX = 'FIREFOX',
+    WEBKIT = 'WEBKIT'
+}
