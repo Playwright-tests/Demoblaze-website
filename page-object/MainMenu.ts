8a3e0223338b3ae879c1d849a734b6a9387b769e
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-04-10 10:13:46 +0200

    Added methods

--- page-object/MainMenu.ts
+++ page-object/MainMenu.ts
@@ -3,13 +3,27 @@ import { BasePage } from "./BasePage";
 
 export class MainMenu extends BasePage {
 
+    private readonly nameOfUserLink: Locator;
+
     constructor(page: Page) {
 
         super(page);
+
+        this.nameOfUserLink = page.locator('#nameofuser');
     }
 
     async clickLink(linkName: string): Promise<void> {
 
         await this.getPage().getByRole('link', {name: linkName}).click();
+    }
+
+    getLinkLocator(linkName: string): Locator {
+
+        return this.getPage().getByRole('link', {name: linkName});
+    }
+
+    getNameOfUserLinkLocator(): Locator {
+
+        return this.nameOfUserLink;
     }
 }
