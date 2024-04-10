91aa9720c7c822ed0241f3138b9202b1c9793580
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-04-10 10:13:15 +0200

    Added methods

--- page-object/LoginForm.ts
+++ page-object/LoginForm.ts
@@ -1,10 +1,55 @@
-import { Page } from "@playwright/test";
+import { Locator, Page } from "@playwright/test";
 import { ModalDialog } from "./ModalDialog";
 
 export class LoginForm extends ModalDialog {
 
+    private readonly usernameField: Locator;
+    private readonly passwordField: Locator;
+    private readonly closeButton: Locator;
+    private readonly loginButton: Locator;
+
     constructor(page: Page) {
 
         super(page, 'Log in');
+
+        this.usernameField = page.locator('#loginusername');
+        this.passwordField = page.locator('#loginpassword');
+        this.closeButton = page.getByRole('button', {name: 'Close'});
+        this.loginButton = page.getByRole('button', {name: 'Log in'});
+    }
+
+    async setUsername(username: string): Promise<void> {
+
+        await this.usernameField.fill(username);
+    }
+
+    async setPassword(password: string): Promise<void> {
+
+        await this.passwordField.fill(password);
+    }
+
+    async getUsername(): Promise<string> {
+
+        return await this.usernameField.inputValue();
+    }
+
+    async getPassword(): Promise<string | null> {
+
+        return await this.passwordField.inputValue();
+    }
+
+    async clickCloseButton(): Promise<void> {
+
+        await this.closeButton.click();
+    }
+
+    async clickLoginButton(): Promise<void> {
+
+        await this.loginButton.click();
+    }
+
+    getLoginButtonLocator(): Locator {
+
+        return this.loginButton;
     }
 }
