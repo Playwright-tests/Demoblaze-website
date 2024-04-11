dc4b77a06de3732737c4cb4995b581dbf481a164
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-04-11 08:45:33 +0200

    Created

---
+++ page-object/productPage.ts
@@ -0,0 +1,44 @@
+import { Locator, Page } from "@playwright/test";
+import { BasePage } from "./BasePage"
+import { ProductData } from "../support/types";
+
+export class ProductPage extends BasePage {
+
+    private readonly column: Locator;
+    private readonly productName: Locator;
+    private readonly productPrice: Locator;
+    private readonly addToCartLink: Locator;
+
+    constructor(page: Page) {
+
+        super(page);
+
+        this.column = page.locator('#tbodyid');
+        this.productName = this.column.locator('.name');
+        this.productPrice = this.column.locator('.price-container');
+        this.addToCartLink = page.getByRole('link', {name: 'Add to cart'});
+    }
+
+    async getProductData(): Promise<ProductData> {
+
+        return {
+            productName: await this.productName.textContent(),
+            productPrice: await this.productPrice.textContent()
+        }
+    }
+
+    async getProductName(): Promise<string | null> {
+
+        return await this.productName.textContent();
+    }
+
+    async getProductPrice(): Promise<string | null> {
+
+        return await this.productPrice.textContent();
+    }
+
+    async clickAddToCartLink(): Promise<void> {
+
+        await this.addToCartLink.click();
+    }
+}
