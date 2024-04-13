c27a3868c9ee118fcafcf9cfd4d9a399d5463652
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-04-13 18:07:07 +0200

    Created config objects

---
+++ src/test/support/config.ts
@@ -0,0 +1,14 @@
+export const browserConfig = {
+
+    browser: process.env.BROWSER || 'CHROMIUM'
+}
+
+export const browserOptions = {
+
+    headless: false
+}
+
+export const objectTypes = {
+
+    object: [] as string[]
+}
