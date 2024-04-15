4665ee494556800c553b9dafdf7ff1f9c34fd45c
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-04-15 09:00:07 +0200

    Added the step

--- src/test/steps/general.ts
+++ src/test/steps/general.ts
@@ -1,9 +1,15 @@
-import { Given } from "@cucumber/cucumber";
+import { Given, Then } from "@cucumber/cucumber";
 import { ICustomWorld } from "../support/customWorld";
 import { URL } from "../../../enums/enums";
+import { expect } from "@playwright/test";
 
 Given('the home page is open',{timeout: 60 * 1000},async function (this: ICustomWorld) {
     
     const { page } = this;
     await page?.goto(URL.HOME_PAGE);
+})
+
+Then('the dialog message should be {string}',async function (this: ICustomWorld, expectedMessage) {
+    
+    expect(this.getMessage()).toEqual(expectedMessage);
 })
