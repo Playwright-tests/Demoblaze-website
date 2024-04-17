b03ae2aa3e39f1aa05926d8f47e78ad083ef0c6e
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-04-17 09:02:02 +0200

    Added the step

--- src/test/steps/general.ts
+++ src/test/steps/general.ts
@@ -1,12 +1,25 @@
-import { Given, Then } from "@cucumber/cucumber";
+import { Given, Then, When } from "@cucumber/cucumber";
 import { ICustomWorld } from "../support/customWorld";
 import { URL } from "../../../enums/enums";
 import { expect } from "@playwright/test";
 
 Given('the home page is open',{timeout: 60 * 1000},async function (this: ICustomWorld) {
     
-    const { page } = this;
-    await page?.goto(URL.HOME_PAGE);
+    await this.page!.goto(this.parameters.baseURL);
+})
+
+Given('a user is logged in',{timeout: 60 * 1000},async function (this: ICustomWorld) {
+    
+    await this.page!.goto(this.parameters.baseURL);
+    await this.mainMenu?.clickLink('Log in'),
+    await this.loginForm?.setUsername('username');
+    await this.loginForm?.setPassword('password');
+    await this.loginForm?.clickLoginButton();
+})
+
+When('user clicks the logo',async function (this: ICustomWorld) {
+    
+    await this.page!.locator('#nava').click();
 })
 
 Then('the dialog message should be {string}',async function (this: ICustomWorld, expectedMessage) {
