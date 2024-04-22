ac8c4645db5f06e655c6e2dfac63aa2c31e9440c
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-04-22 20:25:26 +0200

    Added the step

--- src/test/steps/general.ts
+++ src/test/steps/general.ts
@@ -1,6 +1,7 @@
 import { Given, Then, When } from "@cucumber/cucumber";
 import { ICustomWorld } from "../support/customWorld";
 import { expect } from "@playwright/test";
+import { ModalDialog } from "../../../page-object/ModalDialog";
 
 Given('the home page is open',{timeout: 60 * 1000},async function (this: ICustomWorld) {
     
@@ -25,6 +26,12 @@ Given('the shopping cart is open',async function (this: ICustomWorld) {
 When('user clicks the logo',async function (this: ICustomWorld) {
     
     await this.page!.locator('#nava').click();
+})
+
+Then('the "{string}" form should be displayed',async function (this: ICustomWorld, headerText) {
+  
+    const modaDialog = new ModalDialog(this.page!, headerText);
+    await expect(modaDialog.getBodyLocator()).toBeVisible({timeout: 3000});
 })
 
 Then('the dialog message should be {string}',async function (this: ICustomWorld, expectedMessage) {
