18a3d2175a61caff6a87bfdf8cabdee1ff109418
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-05-18 12:32:23 +0200

    Added steps

--- src/test/steps/placeOrderForm.ts
+++ src/test/steps/placeOrderForm.ts
@@ -5,7 +5,6 @@ import { ICustomWorld } from "../support/customWorld";
 When('a user enters the {string} into the Name field',async function (this: ICustomWorld, name) {
     
     await this.placeOrderForm?.setName(name);
-    await this.page!.waitForTimeout(5000);
 });
 
 When('a user enters the {string} into the Country field',async function (this: ICustomWorld, country) {
@@ -25,7 +24,7 @@ When('a user enters the {string} into the Credit card field',async function (thi
 
 When('a user enters the {string} into the Month field',async function (this: ICustomWorld, month) {
     
-    await this.placeOrderForm?.setName(month);
+    await this.placeOrderForm?.setMonth(month);
 });
 
 When('a user enters the {string} into the Year field',async function (this: ICustomWorld, year) {
@@ -38,3 +37,27 @@ Then('the Name field should be filled with the {string}', async function (this: 
     expect(await this.placeOrderForm!.getName()).toEqual(username);
 });
 
+Then('the Country field should be filled with the {string}', async function (this: ICustomWorld, country) {
+    
+    expect(await this.placeOrderForm!.getCountry()).toEqual(country);
+})
+
+Then('the City field should be filled with the {string}', async function (this: ICustomWorld, city) {
+    
+    expect(await this.placeOrderForm!.getCity()).toEqual(city);
+})
+
+Then('the Credit card field should be filled with the {string}', async function (this: ICustomWorld, creditCard) {
+    
+    expect(await this.placeOrderForm!.getCreditCard()).toEqual(creditCard);
+})
+
+Then('the Month field should be filled with the {string}', async function (this: ICustomWorld, month) {
+    
+    expect(await this.placeOrderForm!.getMonth()).toEqual(month);
+})
+
+Then('the Year field should be filled with the {string}', async function (this: ICustomWorld, year) {
+    
+    expect(await this.placeOrderForm!.getYear()).toEqual(year);
+})
