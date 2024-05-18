77c2e3d79edfa115409894ee7be453b129db3098
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-05-18 11:03:57 +0200

    Created the class

---
+++ page-object/PlaceOrderForm.ts
@@ -0,0 +1,73 @@
+import { Locator, Page } from "@playwright/test";
+import { BasePage } from "./BasePage";
+
+export class PlaceOrderForm extends BasePage {
+
+    private readonly nameField: Locator;
+    private readonly countryField: Locator;
+    private readonly cityField: Locator;
+    private readonly creditCardField: Locator;
+    private readonly monthField: Locator;
+    private readonly yearField: Locator;
+    private readonly closeButton: Locator;
+    private readonly purchaseButton: Locator;
+
+    constructor(page: Page) {
+
+        super(page);
+
+        this.nameField = page.getByLabel('Total');
+        this.countryField = page.getByLabel('Country');
+        this.cityField = page.getByLabel('City');
+        this.creditCardField = page.getByLabel('Credit card');
+        this.monthField = page.getByLabel('Month');
+        this.yearField = page.getByLabel('Year');
+        this.closeButton = page.getByRole('button', {name: 'Close'});
+        this.purchaseButton = page.getByRole('button', {name: 'Purchase'});
+    }
+
+    async setName(name: string): Promise<void> {
+
+        await this.nameField.fill(name);
+    }
+
+    async setCountry(country: string): Promise<void> {
+
+        await this.countryField.fill(country);
+    }
+
+    async setCity(city: string): Promise<void> {
+
+        await this.cityField.fill(city);
+    }
+
+    async setCreditCard(creditCard: string): Promise<void> {
+
+        await this.creditCardField.fill(creditCard);
+    }
+
+    async setMonth(month: string): Promise<void> {
+
+        await this.monthField.fill(month);
+    }
+
+    async setYear(year: string): Promise<void> {
+
+        await this.yearField.fill(year);
+    }
+
+    async clickCloseButton(): Promise<void> {
+
+        await this.closeButton.click();
+    }
+
+    async clickPurchaseButton(): Promise<void> {
+
+        await this.purchaseButton.click();
+    }
+
+    async getName() {
+
+        return await this.nameField.inputValue();
+    }
+}
