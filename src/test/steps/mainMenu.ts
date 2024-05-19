20ecae56569a3e1b8606ec58b0ce96eb86026120
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-05-19 14:46:33 +0200

    Removed quotes

--- src/test/steps/mainMenu.ts
+++ src/test/steps/mainMenu.ts
@@ -4,12 +4,12 @@ import { expect } from "@playwright/test";
 
 setDefaultTimeout(30 * 60 * 1000);
 
-When('a user clicks the "{string}" link', async function (this: ICustomWorld, link) {
+When('a user clicks the {string} link', async function (this: ICustomWorld, link) {
   
     await this.mainMenu?.clickLink(link);
 })
 
-Then('the "{string}" page should be opened',{timeout: 60 * 1000},async function (this: ICustomWorld, url) {
+Then('the {string} page should be opened',{timeout: 60 * 1000},async function (this: ICustomWorld, url) {
     
     await expect(this.page!).toHaveURL(url);
 })
