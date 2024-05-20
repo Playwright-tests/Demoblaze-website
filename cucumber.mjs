4e580a6cb9a4249f3ffdab8ae2184b86fb9f50ef
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-05-20 09:38:03 +0200

    Updated

--- cucumber.mjs
+++ cucumber.mjs
@@ -7,19 +7,27 @@ const config = {
             "snippetInterface": "async-await"
         },
         "paths": [
-            "src/test/features/shoppingCart.feature"
+            "src/test/features/"
         ],
         
         "dryRun": false,
         "require": [
-            "src/test/steps/shoppingCart.ts"
+            "src/**/*.ts"
+
         ],
         "requireModule": [
             "ts-node/register"
         ],
         "parallel": 1,
-}
 
-//config.format.push("file://C:/Projekty/Testowanie/Demoblaze/reporter.js");
+        "worldParameters": { 
+            "baseURL": "https://www.demoblaze.com/index.html",
+            "shoppingCart": "https://www.demoblaze.com/cart.html",
+            "nexus6Link": "Nexus 6",
+            "SamsungGalaxyS6Link": "Samsung galaxy s6"
+        }
+    }
+
+config.format.push("file://C:/Projekty/Testowanie/Demoblaze/reporter.js");
 
 export default config;
